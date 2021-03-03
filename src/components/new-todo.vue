d391d1b0907a20f546efc786d7b9caae4f487ae6
Alexandre <caramel.alexandre@gmail.com>
2021-03-03 18:01:44 +0100

    :rocket:

---
+++ src/components/new-todo.vue
@@ -0,0 +1,42 @@
+<template>
+        <input v-model="taskName" name="add" placeholder="Nouvelle tâche...">
+        <i v-on:click="counter +=1;sendTask()" class="fas fa-plus-circle"></i>
+</template>
+
+<script>
+    export default {
+        name: 'Todo',
+        data () {
+            
+            return {
+                taskname: "",
+                counter: 0,
+            }
+        },
+        methods: {
+            sendTask() {
+                this.$emit('newTask', this.taskName)
+                this.$emit('counter', this.counter)
+            }
+        },
+    }
+</script>
+
+<style>
+  i {
+      cursor: pointer;
+      color: green;
+      font-size: 60px;
+      padding-left: 15px;
+
+  }
+  input {
+      border: solid grey 1px;
+      height: 60px;
+      padding-right: 15px;
+  }
+  input::placeholder {
+      font-size: 18px;
+      text-align: center;
+  }
+</style>
---
+++ src/components/todo-card.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h2>{{ dataDate }}</h2>
+            <h2 id="card-title">VueJs Tutorial Todo List</h2>
+            <h2>{{addition}} tâches</h2>
+        </div>
+        <div class="card-content">
+            <new-todo @counter=count></new-todo>
+        </div>
+    </div>
+</template>
+
+<script>
+import newTodo from './new-todo.vue';
+    export default {
+  components: { newTodo },
+        name: 'TodoList',
+        props: {
+            msg: String
+        },
+        data () {
+            return{
+                tasks : [],
+                addition : 0,
+            }
+        },
+        computed: {
+            dataDate: function(){
+                var date = new Date();
+                let days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
+                let months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
+            
+                var dayNumber = date.getDate();
+                var actualDay = days[date.getDay()]
+                var actualMonth = months[date.getMonth()]
+
+                return actualDay + " " + dayNumber + " " + actualMonth + " "
+            }
+        },
+        methods : {
+            count(b){
+                console.log(b)
+                this.addition=b
+            }
+        }
+    }
+</script>
+
+<style>
+.card {
+    background-color: white !important;
+    border-radius: 10px;
+    font-family: Roboto;
+}
+.card-header {
+    font-size: 22px;
+    justify-content: space-between;
+    padding: 15px;
+    color: black;
+}
+#card-title {
+    color: teal;
+}
+.card-content {
+    display: flex;
+    justify-content: center;
+
+}
+</style>
